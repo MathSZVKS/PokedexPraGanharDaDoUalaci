b43038b9153fe47992c4a089c9558f4374ccf46a
Matheus Sizervinks Ferreira <matheussizervinks56@gmail.com>
2023-03-01 16:43:04 -0300

    pesquisa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,5 @@ export class AppComponent {
   title = 'angular-pokedex';
   url = 'https://media.discordapp.net/attachments/1019237103764250676/1080299040375312444/1200px-International_Pok_C3_A9mon_logo.svg.png';
   class = 'pokemonTitle';
+  classImagem = 'classImagem'
 }
