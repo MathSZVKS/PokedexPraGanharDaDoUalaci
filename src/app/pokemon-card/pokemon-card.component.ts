6f670cc6dbc6e40d5ac82ac3573eea6900caa29b
Matheus Sizervinks Ferreira <matheussizervinks56@gmail.com>
2023-03-02 18:35:47 -0300

    miticos e lendarios

--- src/app/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -17,7 +17,7 @@ export class PokemonCardComponent {
   span = document.getElementsByClassName("close")[0];
 
   @Input() pokemon = '';
-  @Input() lendario: any;
+  @Input() tipo: any;
   @Input() numberOfDex = 0;
   @Input() simpleSearch: any;
   @Input() abilities: any;
--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -12,7 +12,12 @@ export class PokemonListComponent {
   pokemonFinded: any;
   simpleSearch = true;
   abaAtiva = 'pesquisar';
-  lendarios: any;
+  subLendarios = Array();
+  lendarios = Array();
+
+  numDexSubLendarios = ['145','146','243','244','381','480','481','482','485','380','379','378','377','245','281','480','481','482','485','486','488','638','639','640','641','642','645','772','773','785','786','787','788','793','794','795','796','797','798','799','803','805','806','891','892','894','895','896','897','905','1003','1002','1001','1004'];
+  numDexLendario = ['150','249','250','382','383','384','483','484','487','643','644','646','716','717','718','789','790','791','792','800','888','889','890','898','1007','1008'];
+  
 
   constructor(
     public pokemonService: PokemonService,
@@ -20,16 +25,33 @@ export class PokemonListComponent {
   }
 
   ngOnInit(){
-    this.pokemonService.getPokemonByName('151').subscribe(
-      (dados) => {
-        this.lendarios = dados;
-        console.log(this.lendarios);
-      }
-    )
+    this.searchSubLendarios();
+    this.searchLendarios();
+  }
+
+  searchSubLendarios(){
+    this.numDexSubLendarios.forEach((SubLendarioNumDex) => {
+      this.pokemonService.getPokemonByName(SubLendarioNumDex).subscribe(
+        (dados) => {
+          this.subLendarios.push(dados);
+        }
+      )
+    });
+  }
+
+  searchLendarios(){
+    this.numDexLendario.forEach((LendarioNumDex) => {
+      this.pokemonService.getPokemonByName(LendarioNumDex).subscribe(
+        (dados) => {
+          this.lendarios.push(dados);
+        }
+      )
+    });
   }
 
   alterarAbaAtiva(aba: any){
     this.abaAtiva = aba;
+    console.log(this.abaAtiva);
   }
 
   searching(search: string){
