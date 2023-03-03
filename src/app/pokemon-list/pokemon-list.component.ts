bc43cc928baf62fb53923f06d29a02c57142b765
Matheus Sizervinks Ferreira <matheussizervinks56@gmail.com>
2023-03-03 01:42:11 -0300

    miticos

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -14,10 +14,11 @@ export class PokemonListComponent {
   abaAtiva = 'pesquisar';
   subLendarios = Array();
   lendarios = Array();
+  miticos = Array();
 
-  numDexSubLendarios = ['145','146','243','244','381','480','481','482','485','380','379','378','377','245','281','480','481','482','485','486','488','638','639','640','641','642','645','772','773','785','786','787','788','793','794','795','796','797','798','799','803','805','806','891','892','894','895','896','897','905','1003','1002','1001','1004'];
+  numDexSubLendarios = ['145','146','243','244','381','480','481','482','485','380','379','378','377','245','480','481','482','485','486','488','638','639','640','641','642','645','772','773','785','786','787','788','793','794','795','796','797','798','799','803','805','806','891','892','894','895','896','897','905','1003','1002','1001','1004'];
   numDexLendario = ['150','249','250','382','383','384','483','484','487','643','644','646','716','717','718','789','790','791','792','800','888','889','890','898','1007','1008'];
-  
+  numDexMiticos = ['151','251','385','386','489','490','491','492','493','494','647','648','649','719','720','721','801','802','807','808','809','893'];
 
   constructor(
     public pokemonService: PokemonService,
@@ -27,6 +28,7 @@ export class PokemonListComponent {
   ngOnInit(){
     this.searchSubLendarios();
     this.searchLendarios();
+    this.searchMiticos();
   }
 
   searchSubLendarios(){
@@ -44,6 +46,16 @@ export class PokemonListComponent {
       this.pokemonService.getPokemonByName(LendarioNumDex).subscribe(
         (dados) => {
           this.lendarios.push(dados);
+        }
+      )
+    });
+  }
+
+  searchMiticos(){
+    this.numDexMiticos.forEach((MiticoNumDex) => {
+      this.pokemonService.getPokemonByName(MiticoNumDex).subscribe(
+        (dados) => {
+          this.miticos.push(dados);
         }
       )
     });
